733edf08ea7758920d34cf75f1df6d8ab2d28371
Piotr Karasiak <pkarasiak@gmail.com>
2013-04-14 09:12:31 +0200

    Lecture 12: plots.

--- Lecture12/plotWords.py
+++ Lecture12/plotWords.py
@@ -3,21 +3,21 @@ import string
 
 vowels = 'EYUIOA'
 consonants = 'QWRTPSDFGHJKLZXCVBNM'
-vowelCnt = [0,0,0,0,0,0,0,0,0,0]
-consonantsCnt = [0,0,0,0,0,0,0,0,0,0]
+vowelCnt = [0,0,0,0,0,0,0]
+consonantsCnt = [0,0,0,0,0,0,0]
 
 inFile = open('words.txt')
 for line in inFile.readlines():
     word = line.strip()
     for c in word:
         if c in vowels:
-            vowelCnt[len(word) - 1] += 1
+            vowelCnt[len(word)-2] += 1
         if c in consonants:
-            consonantsCnt[len(word) - 1] += 1
+            consonantsCnt[len(word)-2] += 1
 
 pylab.figure(1)
-pylab.plot(range(1, len(vowelCnt)+1), vowelCnt, label='Vowels')
-pylab.plot(range(1, len(consonantsCnt)+1), consonantsCnt, 'r', label='Consonants')
+pylab.plot(range(2, len(vowelCnt)+2), vowelCnt, 'b-o', label='Vowels')
+pylab.plot(range(2, len(consonantsCnt)+2), consonantsCnt, 'r-o', label='Consonants')
 pylab.title('Number of Vowels and Consonants in the words\n of a given length (words.txt from PS4)')
 pylab.xlabel('Word Length')
 pylab.ylabel('Number of Characters')
